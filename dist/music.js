ea310890bdc44d99e4dd4183482b60519b7fbac1
Liweijianf <2364333930@qq.com>
2023-06-25 11:28:57 +0800

    Site updated: 2023-06-25 11:28:56

---
+++ dist/music.js
@@ -0,0 +1,56 @@
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: true,
+  // autoplay: true,
+  audio: [
+    {
+      name: '溯（钢琴版）',
+      artist: '柳轻颂',
+      url: 'http://music.163.com/song/media/outer/url?id=1433584979.mp3',
+      cover: 'http://p1.music.126.net/qX7knUIlpmbJ34UBzCCS6w==/109951164802366812.jpg?param=130y130',
+    },
+    {
+      name: "夢灯籠",
+      artist: 'RADWIMPS',
+      url: '/images/夢灯籠.mp3',
+      cover: 'http://p2.music.126.net/sSxbRt9RpC6s_MaewyDJfA==/18597139672292692.jpg?param=130y130',
+    },
+    {
+      name: '前前前世 (movie ver.)',
+      artist: 'RADWIMPS',
+      url: '/images/前前前世-_movie-ver._-RADWIMPS.mp3',
+      cover: 'http://p2.music.126.net/sSxbRt9RpC6s_MaewyDJfA==/18597139672292692.jpg?param=130y130',
+    },
+    {
+      name: 'Supermarket Flowers',
+      artist: 'Ed Sheeran',
+      url: '/images/Supermarket Flowers.mp3',
+      cover: 'http://p2.music.126.net/99_i681E6ZE74t_xue6PUA==/109951166151204092.jpg?param=130y130',
+    },
+    {
+      name: 'スパークル（火花）——《你的名字。》插曲',
+      artist: 'ゆう十',
+      url: 'http://music.163.com/song/media/outer/url?id=1984045303.mp3',
+      cover: 'http://p2.music.126.net/SiTInUUumsPdSdndv1MCyw==/109951167903888878.jpg?param=130y130',
+    },
+    {
+      name: 'euphoria',
+      artist: ' ',
+      url: 'http://music.163.com/song/media/outer/url?id=1814327255.mp3',
+      cover: '/images/susan1.jpg',
+    },
+    {
+      name: 'Friend',
+      artist: 'Kozoro',
+      url: 'http://music.163.com/song/media/outer/url?id=33004707.mp3',
+      cover: 'http://p2.music.126.net/FR7zKYB8ujNRoWQuGCHbGg==/109951163288692527.jpg?param=130y130',
+    },
+    {
+      name: 'One Last Time',
+      artist: '廖泽蓝',
+      url: 'http://music.163.com/song/media/outer/url?id=1870680152.mp3',
+      cover: 'http://p1.music.126.net/Y_Io_ln1-ujiL9eFdyQvmQ==/109951166291345875.jpg?param=130y130',
+    }
+  ]
+});
+​
